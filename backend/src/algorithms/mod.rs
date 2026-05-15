487c62590def37c599270f972d956cae9b05c50c
MrmAya <kahlimeriemaya@gmail.com>
2026-05-15 19:46:30 +0200

    chore(algorithms): add module exports

---
+++ backend/src/algorithms/mod.rs
@@ -0,0 +1,16 @@
+#[allow(dead_code)]
+pub mod asymmetric;
+#[allow(dead_code)]
+pub mod classic;
+#[allow(dead_code)]
+pub mod comms;
+#[allow(dead_code)]
+pub mod hash;
+#[allow(dead_code)]
+pub mod homomorphic;
+#[allow(dead_code)]
+pub mod signature;
+#[allow(dead_code)]
+pub mod secret_sharing;
+#[allow(dead_code)]
+pub mod symmetric;
